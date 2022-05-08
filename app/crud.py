fbccbdedf40c0ea21c982ebabaaa5954578a6a79
FernandoCelmer <email@fernandocelmer.com>
2022-05-07 21:15:53 -0300

    Delete code debug

--- app/crud.py
+++ app/crud.py
@@ -7,8 +7,6 @@ def get_items(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Item).offset(skip).limit(limit).all()
 
 def create_item(db: Session, item: schemas.ItemCreate):
-    from pdb import set_trace; set_trace()
-
     db_item = models.Item(**item.dict())
     db.add(db_item)
     db.commit()
