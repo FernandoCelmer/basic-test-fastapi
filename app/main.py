09fa6ee83f3a38d5ea23e5d54c09de3abad595b4
FernandoCelmer <email@fernandocelmer.com>
2022-05-16 00:55:45 -0300

    No Task - Update routes

--- app/main.py
+++ app/main.py
@@ -1,30 +1,16 @@
-from typing import List
-
-from fastapi import Depends, FastAPI, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from sqlalchemy.orm import Session
-
-from . import crud, models, schemas
-from app.database import SessionLocal, engine
+from app.api.v1.routers import router
 from mangum import Mangum
 
-models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+app.include_router(router, prefix="/v1")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app.mount("/app/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
@@ -33,33 +19,6 @@ async def root(request: Request):
         {
             "request": request,
             "message": "project-basic-test-fastapi"
-        }
-    )
-
-@app.get("/items/", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
-
-@app.post("/items/", response_model=schemas.Item)
-def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
-    return crud.create_item(db=db, item=item)
-
-@app.get("/items/{id}", response_model=schemas.Item)
-def read_item(id: str, db: Session = Depends(get_db)):
-    item = crud.get_item(db=db, id=id)
-    return item
-
-@app.get("/view_item/{id}", response_class=HTMLResponse)
-async def view_item(request: Request, id: str, db: Session = Depends(get_db)):
-    teste = crud.get_item(db=db, id=id)
-    return templates.TemplateResponse(
-        "item.html",
-        {
-            "request": request,
-            "id": teste.id,
-            "title": teste.title,
-            "description": teste.description
         }
     )
 
