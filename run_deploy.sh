bc2a56a213f4b651b1fd84d78af8ed7a0051e604
FernandoCelmer <email@fernandocelmer.com>
2022-05-16 00:15:55 -0300

    No Task - Add script run deploy

---
+++ run_deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+root_dir=$PWD
+venv_dir="$root_dir/venv/lib/python3.9/site-packages"
+
+# Zip Package
+cd $venv_dir && zip -r9 "$root_dir/lambda.zip" . \
+&& cd "$root_dir/api" && zip -g ../lambda.zip -r .
+
+# Upload S3
+bucket_name="upy-fastapi-lambda-dev"
+cd $root_dir
+aws s3 cp lambda.zip s3://$bucket_name/lambda.zip --profile upy
+
+# Update Function Lambda
+function_name="upy-fastapi-lambda-dev"
+bucket="upy-fastapi-lambda-dev"
+aws lambda update-function-code --function-name $function_name --s3-bucket $bucket --s3-key lambda.zip --profile upy
