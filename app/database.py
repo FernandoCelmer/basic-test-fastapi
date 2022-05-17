2a9a1f0b83dd84b6c715b7fb7a0305bfc03c50ce
FernandoCelmer <email@fernandocelmer.com>
2022-05-17 09:31:13 -0300

    No Task - Include MySQL SQLALCHEMY_DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(
