e9a5ae60a76ce9a173993eda15c57f6646db067b
FernandoCelmer <email@fernandocelmer.com>
2022-09-18 00:09:37 -0300

    No Task - Update API

--- app/api/v1/endpoints/items.py
+++ app/api/v1/endpoints/items.py
@@ -5,23 +5,25 @@ from fastapi import Depends, Request, APIRouter
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from app.controllers.item import ControlerItem
-from app.schemas.item import SchemaItem
-from app.database import get_db
+from app.core.controllers.item import ControlerItem
+from app.core.schemas.item import SchemaItem
+from app.settings.database import get_db
 
 
 router = APIRouter()
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory="app/static/templates")
 
 
 @router.get("/items", response_model=List[SchemaItem])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    operation = ControlerItem(db=db)
+    """Get all items from the database."""
+    operation = ControlerItem(db=db,)
     return operation.get_all()
 
 
 @router.get("/items/{id}", response_model=SchemaItem)
 def read_item(id: str, db: Session = Depends(get_db)):
+    """Get an item from the database by ID."""
     operation = ControlerItem(db=db)
     return operation.get(model_id=id)
 
