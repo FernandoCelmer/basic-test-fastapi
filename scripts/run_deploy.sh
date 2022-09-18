3166c08da7bdd99be12db352c5bbbae4652cea38
FernandoCelmer <email@fernandocelmer.com>
2022-09-17 21:37:11 -0300

    No Task - Update scripts

--- scripts/run_deploy.sh
+++ scripts/run_deploy.sh
@@ -2,19 +2,20 @@
 
 root_dir=$PWD
 venv_dir="$root_dir/venv/lib/python3.9/site-packages"
+function_name="upy-fastapi-lambda-dev"
+bucket_name="upy-fastapi-lambda-dev"
+file_name="upy-0.0.1.zip"
+
 
 # Zip Package
-mkdir zip && cp -r app/ zip/app/ \
-&& cd $venv_dir && zip -r9 "$root_dir/lambda.zip" . \
-&& cd "$root_dir/zip" && zip -g ../lambda.zip -r . \
+mkdir zip && cp -r app/ zip/app/ && cp -r config.py zip/config.py && cp -r sql_app.db zip/sql_app.db \
+&& cd $venv_dir && zip -r9 "$root_dir/$file_name" . \
+&& cd "$root_dir/zip" && zip -g ../$file_name -r . \
 && cd "$root_dir" && rm -r zip
 
 # Upload S3
-bucket_name="upy-fastapi-lambda-dev"
 cd $root_dir
-aws s3 cp lambda.zip s3://$bucket_name/lambda.zip --profile upy
+aws s3 cp $file_name s3://$bucket_name/$file_name --profile upy
 
-# Update Function Lambda
-function_name="upy-fastapi-lambda-dev"
-bucket="upy-fastapi-lambda-dev"
-aws lambda update-function-code --function-name $function_name --s3-bucket $bucket --s3-key lambda.zip --profile upy
+# Update Lambda Function 
+aws lambda update-function-code --function-name $function_name --s3-bucket $bucket_name --s3-key $file_name --profile upy
